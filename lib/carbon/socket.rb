239c1982769b176810b5133df7e0b2e68a1e62a4
Mathieu Martin <mathieu.martin@lightspeedretail.com>
2014-04-11 16:35:20 -0400

    Initial

---
+++ lib/carbon/socket.rb
@@ -0,0 +1,7 @@
+require "carbon/socket/version"
+
+module Carbon
+  module Socket
+    # Your code goes here...
+  end
+end
---
+++ lib/carbon/socket/version.rb
@@ -0,0 +1,5 @@
+module Carbon
+  module Socket
+    VERSION = "0.0.1"
+  end
+end
